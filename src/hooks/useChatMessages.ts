1bb841a191acded0831f9549b1dd95e963bb9712
umemiya0715 <whiteblue116@icloud.com>
2024-10-16 12:35:42 +0900

    consistent-chat

--- src/hooks/useChatMessages.ts
+++ src/hooks/useChatMessages.ts
@@ -1,8 +1,8 @@
 import { atom, useAtom } from "jotai";
 
 type ChatMessages = {
-  text: string;
-  isUser: boolean;
+  content: string;
+  role: "user" | "assistant";
 };
 
 const chatAtom = atom<ChatMessages[]>([]);
@@ -10,13 +10,33 @@ const chatAtom = atom<ChatMessages[]>([]);
 export const useChatMessages = () => {
   const [chatMessages, setChatMessages] = useAtom(chatAtom);
 
-  const addChatMessages = (ChatMessages: ChatMessages) => {
-    setChatMessages(prev => [...prev, ChatMessages]);
+  const addUserMessages = (input: string) => {
+    const newMessage: ChatMessages = { content: input, role: "user" };
+    setChatMessages(prev => [...prev, newMessage]);
+  };
+
+  const addAiMessages = (input: string) => {
+    const newMessage: ChatMessages = { content: input, role: "assistant" };
+    setChatMessages(prev => [...prev, newMessage]);
   };
 
   const clearMessages = () => {
     setChatMessages([]);
   };
 
-  return { chatMessages, addChatMessages, clearMessages };
+  function createChatContext(input: string) {
+    // 会話の一貫性を持たせるため、最新の10件(暫定)のメッセージを取得して、新しいメッセージを追加する
+    const recentMessages = chatMessages.slice(-10);
+    const newMessage: ChatMessages = { content: input, role: "user" };
+    recentMessages.push(newMessage);
+
+    const context = recentMessages.map(message => ({
+      content: message.content,
+      role: message.role,
+    }));
+
+    return context;
+  }
+
+  return { chatMessages, addUserMessages, addAiMessages, clearMessages, createChatContext };
 };
